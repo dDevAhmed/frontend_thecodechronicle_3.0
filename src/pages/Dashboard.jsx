613b7064023f3acb043e01974627f36c5fb45b02
Ahmed <ahmedmahmud1994@hotmail.com>
2024-10-05 01:26:09 +0100

    dashboard - sales details section

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import { IoIosTrendingUp, IoIosTrendingDown } from "react-icons/io";
 import { HiMiniUsers, HiMiniCube } from "react-icons/hi2";
 import { BiLineChart } from "react-icons/bi";
 import { PiClockCounterClockwiseFill } from "react-icons/pi";
+// eslint-disable-next-line no-unused-vars
+import { ResponsiveContainer, AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Area } from "recharts"
 
 const dashboardData = [
   {
@@ -15,7 +17,7 @@ const dashboardData = [
           bg: '#8280FF'
         },
         growth: {
-          icon: 'progress',
+          name: 'progress',
           rate: 8.5,
           time: 'yesterday'
         }
@@ -28,7 +30,7 @@ const dashboardData = [
           bg: '#FEC53D'
         },
         growth: {
-          icon: 'progress',
+          name: 'progress',
           rate: 1.3,
           time: 'pass week'
         }
@@ -41,7 +43,7 @@ const dashboardData = [
           bg: '#4AD991'
         },
         growth: {
-          icon: 'regress',
+          name: 'regress',
           rate: 4.3,
           time: 'yesterday'
         }
@@ -54,10 +56,62 @@ const dashboardData = [
           bg: '#FF9066'
         },
         growth: {
-          icon: 'progress',
+          name: 'progress',
           rate: 1.8,
           time: 'yesterday'
         }
+      },
+    ]
+  },
+  {
+    sales: [
+      {
+        sales: '5k',
+        percentage: 10,
+      },
+      {
+        sales: '10k',
+        percentage: 50,
+      },
+      {
+        sales: '15k',
+        percentage: 10,
+      },
+      {
+        sales: '20k',
+        percentage: 99,
+      },
+      {
+        sales: '25k',
+        percentage: 0,
+      },
+      {
+        sales: '30k',
+        percentage: 35,
+      },
+      {
+        sales: '35k',
+        percentage: 70,
+      },
+      {
+        sales: '40k',
+        percentage: 15,
+      },
+      {
+        sales: '45k',
+        percentage: 35,
+      },
+      {
+        sales: '50k',
+        percentage: 8,
+      },
+      {
+        sales: '55k',
+        percentage: 25,
+      },
+      {
+        sales: '60k',
+        percentage: 10,
       },
     ]
   }
@@ -87,15 +141,38 @@ const Dashboard = () => {
             </div>
             <div className="mt-5">
               <p className="flex items-center gap-2 text-[.8125rem]">
-                <span className={`flex items-center gap-2 ${stat.growth.icon === 'progress' ? 'text-[#00B69B]' : 'text-[#F93C65]'}`}>
-                  {stat.growth.icon === 'progress' ? <IoIosTrendingUp /> : <IoIosTrendingDown />}
+                <span className={`flex items-center gap-2 ${stat.growth.name === 'progress' ? 'text-[#00B69B]' : 'text-[#F93C65]'}`}>
+                  {stat.growth.name === 'progress' ? <IoIosTrendingUp /> : <IoIosTrendingDown />}
                   {stat.growth.rate}%
                 </span>
-                <span className="text-[#60606]">{stat.growth.icon === 'progress' ? <span>Up</span> : <span>Down</span>} from {stat.growth.time}</span>
+                <span className="text-[#606060]">{stat.growth.name === 'progress' ? <span>Up</span> : <span>Down</span>} from {stat.growth.time}</span>
               </p>
             </div>
           </Card>
         ))}
+      </div>
+      <div className="mt-5">
+        <ResponsiveContainer width="100%">
+          <Card>
+            <h3 className="text-[#202224] text-[1.125rem] font-semibold mb-5">Sales Details</h3>
+            {/* //fixme - mobile responsiveness */}
+            <AreaChart width={900} height={250} data={dashboardData[1].sales}
+              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+              <defs>
+                <linearGradient id="colorPercentage" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#42B6F6" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#BFE8FF" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis dataKey="sales" />
+              <YAxis domain={[0, 100]} ticks={[20, 40, 60, 80, 100]} />
+              <CartesianGrid strokeDasharray="3 3" />
+              <Tooltip />
+              <Legend />
+              <Area type="monotone" dataKey="percentage" stroke="#4379EE" fillOpacity={1} fill="url(#colorPercentage)" />
+            </AreaChart>
+          </Card>
+        </ResponsiveContainer>
       </div>
     </>
   )
