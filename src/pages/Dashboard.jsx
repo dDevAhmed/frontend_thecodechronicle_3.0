2f0895d7b440eafcd06b60b69705900d80ef2080
Ahmed <ahmedmahmud1994@hotmail.com>
2024-10-15 21:12:43 +0100

    dashboard stat - sales chat

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,6 +13,7 @@ import FeaturedProductImage from '../assets/images/products/product-10.jpg'
 import AppleWatchImage from '../assets/images/products/product-9.jpg'
 import PagesTitle from "../components/PagesTitle";
 import RevenueChart from "../components/charts/RevenueChart";
+import SalesChart from "../components/charts/SalesChart";
 
 const dashboardData = [
   {
@@ -221,32 +222,14 @@ const Dashboard = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5 mt-5">
         {dashboardData[0].stats.map((stat, index) => (
-          <DashboardStat key={index} stat={stat}/>
+          <DashboardStat key={index} stat={stat} />
         ))}
       </div>
-      <Card classNames={'px-4 py-5 sm:p-6 mt'} style={{ backgroundColor: '#fff', marginTop: '20px' }}>
-        <span className="flex items-center justify-between  mb-5">
-          <h3 className="text-brand-primary-black text-[1.125rem] font-semibold">Sales Details</h3>
-          <Dropdown />
-        </span>
-        <ResponsiveContainer width='100%' height={300}>
-          <AreaChart data={dashboardData[1].sales}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-            <defs>
-              <linearGradient id="colorPercentage" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#42B6F6" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#BFE8FF" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="sales" />
-            <YAxis domain={[0, 100]} ticks={[20, 40, 60, 80, 100]} />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Legend />
-            <Area type="monotone" dataKey="sales" stroke="#4379EE" fillOpacity={1} fill="url(#colorPercentage)" />
-          </AreaChart>
-        </ResponsiveContainer>
-      </Card>
+
+      <div className="mt-5">
+        <SalesChart salesData={dashboardData[1].sales}/>
+      </div>
+
       <Card classNames={'px-4 py-5 sm:p-6'} style={{ backgroundColor: '#fff', marginTop: '20px' }}>
         <span className="flex items-center justify-between  mb-5">
           <h3 className="text-brand-primary-black text-[1.125rem] font-semibold">Deals Details</h3>
