7cc8fc24d53c09574ee5d9b58a44acf3e9e9886b
Ahmed <ahmedmahmud1994@hotmail.com>
2024-10-16 02:10:01 +0100

    contact card component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,7 @@
-import { MdMailOutline } from "react-icons/md";
-import Card from "../ui/Card"
-import Button from "../ui/Button"
 import PagesTitle from "../components/PagesTitle";
+import ContactPerson from "../components/cards/ContactPerson";
 
-const contact = [
+const contactData = [
   {
     image: 'https://i.pravatar.cc/500?img=68',
     name: 'Jason Price',
@@ -43,15 +41,8 @@ const Contact = () => {
 
       <div className="mt-5 grid grid-cols-1 md:grid-cols-3 gap-5">
         {
-          contact.map((contact, index) => (
-            <Card key={index} style={{ backgroundColor: '#fff', display: 'relative', padding: 'none' }}>
-              <img src={contact.image} alt='contact image' className="mb-5 w-full" />
-              <span className="text-center">
-                <p className="text-brand-primary-black font-medium">{contact.name}</p>
-                <p className="text-brand-primary-black text-opacity-50 text-[.875rem]">{contact.email}</p>
-                <Button classNames={'my-5 mx-auto px-4 py-2 flex items-center gap-2 border border-[#979797] text-[#979797] hover:bg-brand-primary-blue hover:text-white hover:border-brand-primary-blue'}><MdMailOutline /> Message</Button>
-              </span>
-            </Card>
+          contactData.map((contact, index) => (
+            <ContactPerson key={index} person={contact} />
           ))
         }
       </div>
